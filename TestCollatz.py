54904b4b490d9fc6d7ce84e8badfbebf2c1ad5d4
Samuel Morris <sfmorris@gmail.com>
2015-06-17 11:47:51 -0500

    added some hulked up unit testing

--- TestCollatz.py
+++ TestCollatz.py
@@ -15,7 +15,7 @@
 from io       import StringIO
 from unittest import main, TestCase
 
-from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve, collatz_cycle_length
 
 # -----------
 # TestCollatz
@@ -26,11 +26,23 @@ class TestCollatz (TestCase) :
     # read
     # ----
 
-    def test_read (self) :
+    def test_read_1 (self) :
         s    = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
+
+    def test_read_2 (self) :
+        s   = "1 999999\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 1)
+        self.assertEqual(j, 999999)
+
+    def test_read_3 (self) :
+        s = "420 69"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 420)
+        self.assertEqual(j, 69)
 
     # ----
     # eval
@@ -52,24 +64,75 @@ class TestCollatz (TestCase) :
         v = collatz_eval(900, 1000)
         self.assertEqual(v, 174)
 
+    def test_eval_5 (self) :
+        v = collatz_eval(1000, 900)
+        self.assertEqual(v, 174)
+
+    def test_eval_6 (self) :
+        v = collatz_eval(999998, 999999)
+        self.assertEqual(v, 259)
+
+    def test_eval_7 (self) :
+        v = collatz_eval(350000, 650000)
+        self.assertEqual(v, 509)
+
+    # ------------
+    # cycle_length
+    # ------------
+
+    def test_cycle_length_1 (self) :
+        v = collatz_cycle_length(1)
+        self.assertEqual(v, 1)
+
+    def test_cycle_length_2 (self) :
+        v = collatz_cycle_length(2)
+        self.assertEqual(v, 2)
+
+    def test_cycle_length_3 (self) :
+        v = collatz_cycle_length(3)
+        self.assertEqual(v, 8)
+
+    def test_cycle_length_4 (self) :
+        v = collatz_cycle_length(999997)
+        self.assertEqual(v, 114)
+
+    def test_cycle_length_5 (self) :
+        v = collatz_cycle_length(999998)
+        self.assertEqual(v, 259)
+
+    def test_cycle_length_6 (self) :
+        v = collatz_cycle_length(999999)
+        self.assertEqual(v, 259)
+
     # -----
     # print
     # -----
 
-    def test_print (self) :
+    def test_print_1 (self) :
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
+
+    def test_print_2 (self) :
+        w = StringIO()
+        collatz_print(w, 999999, 1, 20)
+        self.assertEqual(w.getvalue(), "999999 1 20\n")
 
     # -----
     # solve
     # -----
 
-    def test_solve (self) :
+    def test_solve_1 (self) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
         self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2 (self) :
+        r = StringIO("10 1\n200 100\n201 180\n900 9999\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(w.getvalue(), "10 1 20\n200 100 125\n201 180 120\n900 9999 262\n")
 
 # ----
 # main
